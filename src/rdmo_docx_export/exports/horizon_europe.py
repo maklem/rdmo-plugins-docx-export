709602cb97b598636a9a3031f4dd1b50d0909cc7
Marco Klement <marco.klement@fau.de>
2026-06-03 14:17:56 +0200

    first cells filled

--- src/rdmo_docx_export/exports/horizon_europe.py
+++ src/rdmo_docx_export/exports/horizon_europe.py
@@ -1,10 +1,14 @@
 import io
-
 from importlib import resources
-from django.http import HttpResponse
 
+from docx.enum.text import WD_BREAK, WD_ALIGN_PARAGRAPH
+from docx.styles.style import CharacterStyle
+from docx.text.paragraph import Paragraph
 from docx import Document
 from docx.table import Table
+
+from django.http import HttpResponse
+from django.utils import translation
 
 from rdmo.projects.exports import Export
 
@@ -15,21 +19,64 @@ class HorizonEuropeDocxExport(Export):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    def _a1a(self, datasets, para: Paragraph):
+        para.text = ""
+        para.alignment = WD_ALIGN_PARAGRAPH.LEFT
+
+        first = True
+        for data in datasets:
+            if not first:
+                para.add_run("\n\n")
+            first = False
+
+            origin = self.get_value("project/dataset/origin", set_index=data.set_index)
+
+            headline = para.add_run(f"Dataset {data.value}")
+            headline.italic = True
+            headline.add_break()
+            para.add_run(f"This dataset is {origin.value.lower()}.").add_break()
+            para.add_run(self.get_text("project/dataset/usage_description", set_index=data.set_index))
+
     def render(self):
+
         template = resources.files(templates) / "horizon-template.docx"
         doc = Document(template.open("rb"))
-        
-        for para in doc.paragraphs:
-            if para.text.startswith("{{") and para.text.endswith("}}"):
-                # print(f"Found template section {para.text}")
-                para.text = "Lorem Ipsum..."
+        with translation.override("en"):
+            datasets = self.get_set("project/dataset/id")
+            partners = self.get_set("project/partner/id")
+            funders = self.get_set("project/funder/id")
 
-        for tab in doc.tables:
-            for c in tab.columns:
-                for cell in c.cells:
-                    if cell.text.startswith("{{") and cell.text.endswith("}}"):
-                        print(f"Found template section {cell.text} in table")
-                        cell.text = "Lorem Ipsum..."
+            replacements = {
+                "{{projectnumber}}": self.get_text("project/funder/grant_nr"),
+                "{{projectacronym}}": self.get_text("project/acronym"),
+                "{{projecttitle}}": self.get_text("project/title"),
+                "{{dmpdate}}": self.get_text("project/dmp/dmp_date"),
+                "{{dmpversion}}": self.get_text("project/dmp/dmp_version"),
+                "{{Answer1a}}": self._a1a,
+            }
+
+            for para in doc.paragraphs:
+                if para.text.startswith("{{") and para.text.endswith("}}"):
+                    if para.text in replacements:
+                        if isinstance(replacements[para.text], str):
+                            para.text = replacements[para.text]
+                        else:
+                            replacements[para.text](datasets, para)
+                    else:
+                        para.text = "Lorem Ipsum..."
+
+            for tab in doc.tables:
+                for c in tab.columns:
+                    for cell in c.cells:
+                        if cell.text.startswith("{{") and cell.text.endswith("}}"):
+                            print(f"Found template section {cell.text} in table")
+                            if cell.text in replacements and replacements[cell.text] is not None:
+                                if isinstance(replacements[cell.text], str):
+                                    cell.text = replacements[cell.text]
+                                else:
+                                    replacements[cell.text](datasets, cell)
+                            else:
+                                cell.text = "Lorem Ipsum..."
 
         response_data = io.BytesIO()
         doc.save(response_data)
